d969886d9bcaac7cdc3ac784d7dfed5cb64177ba
Tom Beckmann <tomjonabc@gmail.com>
2017-11-24 22:41:20 +0100

    Tweak airblast shape

--- effects/airblast/airblast.gd
+++ effects/airblast/airblast.gd
@@ -1,18 +1,14 @@
 extends Node2D
 
-const AREA_LENGTH = 700
+const FALLOFF = 100
 const PUSH_FORCE = 2000
-
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
 
 func emit():
 	$particles.restart()
 	for body in $influence_area.get_overlapping_bodies():
 		var distance = global_position.distance_to(body.global_position)
-		var strength = distance / AREA_LENGTH
+		var strength = FALLOFF / distance
+		print(strength)
 		var direction = (body.global_position - global_position).normalized()
 		if body.has_method("add_force"):
-			body.add_force(Vector2(0,0), direction * strength * PUSH_FORCE)
+			body.apply_impulse(Vector2(0,0), direction * strength * PUSH_FORCE)
