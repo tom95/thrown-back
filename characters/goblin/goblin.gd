2d1592f1d68393d49a9c4761e1239d25f4fd32b0
Corinna Jaschek <corinna.jaschek@gmail.com>
2017-11-24 23:04:33 +0100

    make goblin throw stones at you

---
+++ characters/goblin/goblin.gd
@@ -0,0 +1,34 @@
+extends "res://characters/npc_bouncer.gd"
+
+const COOLDOWN = 1000
+const STONE_SPEED = 100
+var stone_cooldown = 0
+var attacking = false
+
+onready var stone_spawn = get_node("base/stone_spawn")
+onready var wizard = get_node("../../wizard")
+
+func _ready():
+	pass
+
+func _process(delta):
+	stone_cooldown = max(0, stone_cooldown - 1000 * delta)
+	if attacking and stone_cooldown <= 0:
+		throw_stone()
+		
+func throw_stone():
+	print("throwing")
+	stone_cooldown = COOLDOWN
+	var projectile = preload("res://effects/stone/stone.tscn").instance()
+	projectile.add_collision_exception_with(self)
+	projectile.linear_velocity = (wizard.global_position - stone_spawn.global_position) * 2
+	projectile.position = stone_spawn.global_position
+	get_parent().get_parent().add_child(projectile)
+
+func _on_attack_area_body_entered( body ):
+	if (body is preload("res://characters/wizard/wizard.gd")):
+		attacking = true
+
+func _on_attack_area_body_exited( body ):
+	if (body is preload("res://characters/wizard/wizard.gd")):
+		attacking = false
---
+++ effects/stone/stone.gd
@@ -0,0 +1,4 @@
+extends RigidBody2D
+
+func _on_Timer_timeout():
+	queue_free()
