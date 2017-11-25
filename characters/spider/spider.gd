9eac0bff53b595c3e07691fcc3a22605276a81b8
Eva <drache555@gmx.de>
2017-11-25 22:51:01 +0100

    minor spider adjusments

--- characters/spider/spider.gd
+++ characters/spider/spider.gd
@@ -1,7 +1,7 @@
 extends "res://characters/npc_bouncer.gd"
 
 const DAMAGE = 100
-const COOLDOWN = 3000
+const COOLDOWN = 2400
 var attack_cooldown = 0
 var attacking = false
 var attackee = null
@@ -23,7 +23,6 @@ func _process(delta):
 
 func attack():
 	attack_cooldown = COOLDOWN
-	print("spiderattack")
 	linear_velocity =  Vector2(linear_velocity.x +(attackee.global_position.x - global_position.x), linear_velocity.y)
 
 func _on_attack_area_body_entered( body ):
