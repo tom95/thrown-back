9681010a7463662f07625b9200427b1d88690cf9
Tom Beckmann <tomjonabc@gmail.com>
2017-11-26 15:50:05 +0100

    Simplify spawn logic

--- levels/level.gd
+++ levels/level.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-signal spawn(object)
-
 func _ready():
 	for character in $characters.get_children():
 		connect_character_signals(character)
@@ -10,11 +8,7 @@ func connect_character_signals(character):
 	character.connect("spawn", self, "_on_character_wants_spawn")
 
 func _on_character_wants_spawn(object):
-	# check if we are inside a game or if we are being run by ourselves
-	if get_tree().get_root().get_child(0) == self:
-		add_child(object)
-	else:
-		emit_signal("spawn", object)
+	add_child(object)
 
 func get_spawn_position():
 	return $spawn.global_position
