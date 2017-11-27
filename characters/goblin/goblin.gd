c48abd630e02bda5af59058c7f99a79d2a70e8a9
Corinna Jaschek <corinna.jaschek@gmail.com>
2017-11-27 17:23:16 +0100

    make goblin stones despwan on hit, make weapon timeout higher

--- characters/goblin/goblin.gd
+++ characters/goblin/goblin.gd
@@ -20,6 +20,7 @@ func throw_stone():
 	stone_cooldown = COOLDOWN
 	var projectile = preload("res://effects/stone/stone.tscn").instance()
 	projectile.add_collision_exception_with(self)
+	projectile.target_group = "players"
 	projectile.linear_velocity = (attacking.global_position - stone_spawn.global_position) * 2.5
 	projectile.position = stone_spawn.global_position
 	emit_signal("spawn", projectile)
--- effects/stone/stone.gd
+++ effects/stone/stone.gd
@@ -1,12 +1,11 @@
-extends RigidBody2D
+extends "res://effects/damaging_projectile.gd"
 
-const DAMAGE = 100
-var has_hit = false
+func get_damage(body):
+	return 100
 
-func _on_Timer_timeout():
+func disable_particles():
+	pass
+
+func _on_body_entered( body ):
+	._on_body_entered( body )
 	queue_free()
-
-func _on_damage_area_body_entered( body ):
-	if body.is_in_group("players") and not has_hit:
-		has_hit = true
-		body.take_damage(DAMAGE, $stone.texture)
