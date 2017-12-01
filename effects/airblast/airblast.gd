ae7ab88cabc0d7543f97e9f358a77668e1b9ed88
Tom Beckmann <tomjonabc@gmail.com>
2017-12-01 17:50:27 +0100

    Add cave-like structure at the end of lvl1

--- effects/airblast/airblast.gd
+++ effects/airblast/airblast.gd
@@ -9,7 +9,6 @@ func emit():
 	for body in $influence_area.get_overlapping_bodies():
 		var distance = global_position.distance_to(body.global_position)
 		var strength = FALLOFF / max(distance, MAX_STRENGTH_BOUND)
-		print(strength)
 		var direction = (body.global_position - global_position).normalized()
 		if body.has_method("apply_impulse"):
 			body.apply_impulse(Vector2(0,0), direction * strength * PUSH_FORCE)
